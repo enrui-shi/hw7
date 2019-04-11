13c29b9762159a1056f50e1521cffac505166ef4
stone <enrui.shi@stonybrook.edu>
2019-04-11 12:00:04 -0400

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'username',
-  password : 'password',
+  password : '19980104',
   database : 'hw7'
 });
 connection.connect();
