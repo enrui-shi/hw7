4d727a122ad2499bca2b9245cf0eed26a70ced79
stone <enrui.shi@stonybrook.edu>
2019-04-11 11:55:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '19980104',
+  database : 'hw7'
+});
+connection.connect();
+if(connection.state === 'disconnected'){
+    console.log('error')
+  }else{
+      console.log('db connected')
+  }
+
+app.get('/hw7',function(req,res){
+    club = req.query.club
+    pos = req.query.pos
+    res.json({
+        'club':club,
+        'pos':pos,
+
+    })
+})
+app.listen(port,'0.0.0.0', () => {
+    return console.log(`App listening on port ${port}!`);
+})
